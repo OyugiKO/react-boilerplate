3c03f3a008ec0adf9071b6774b5d08c042f87a1e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-30 07:54:13 +0000

    Deploy OyugiKO/react-boilerplate to github.com/OyugiKO/react-boilerplate.git:gh-pages

---
+++ precache-manifest.a94688842fbf2fee75e9c85bf8cd539a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69eae1372a82f3cee48f",
+    "url": "/OyugiKO/react-boilerplate/static/css/main.406f6ced.chunk.css"
+  },
+  {
+    "revision": "69eae1372a82f3cee48f",
+    "url": "/OyugiKO/react-boilerplate/static/js/main.69eae137.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/OyugiKO/react-boilerplate/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "3ae9a9b8f269260fe3ec",
+    "url": "/OyugiKO/react-boilerplate/static/js/runtime~main.3ae9a9b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/OyugiKO/react-boilerplate/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "69d5a12412e36d78fb57aa8d5104c6e0",
+    "url": "/OyugiKO/react-boilerplate/index.html"
+  }
+];
