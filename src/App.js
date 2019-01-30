9a6e27246dda05fe53f6da90690487c0573a4f78
OyugiKO <kevin.owino@andela.com>
2019-01-30 10:48:07 +0300

    added some data on App.js

--- src/App.js
+++ src/App.js
@@ -6,23 +6,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <p>
+      Proident id excepteur ut aliquip eiusmod ex ut occaecat aliqua fugiat consectetur labore. Est consequat ut ad aute id qui ut. Veniam amet elit laborum esse qui duis aliquip.
+      </p>
+      <br>
+      Nostrud veniam minim qui mollit sint velit occaecat. Quis dolor esse cupidatat nisi amet cillum pariatur ex officia ut aute commodo dolore. Cillum ea anim quis ipsum consectetur ullamco irure Lorem ullamco aliquip irure voluptate. Nisi adipisicing excepteur velit adipisicing mollit commodo adipisicing eiusmod. Ex deserunt adipisicing nostrud proident laborum do Lorem consequat velit veniam amet. Velit Lorem nulla ad tempor officia est est magna.
+      
+      Sint enim pariatur excepteur qui irure est laboris. Duis irure adipisicing voluptate voluptate proident minim irure reprehenderit. Fugiat ullamco cillum ipsum dolor deserunt ad magna aute nisi ad irure consequat ex labore. Duis labore velit nisi cupidatat.
+      </br>
+      <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+      Learn React
+      </a>
+      </header>
       </div>
-    );
+      );
+    }
   }
-}
-
-export default App;
+  
+  export default App;
+  
